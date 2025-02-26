f586eb777dea0abe9d9d5bc96124bb4cfde47dcc
Ignacio-fabianamaria <faby.tbe@gmail.com>
2025-02-25 21:33:50 -0300

    add routes and angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MovieContainerComponent } from './movie/movie-container/movie-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'filmes', component:MovieContainerComponent}
+];
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { Router} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private router: Router){
 
+  }
+navigateTo(rota: string):void{
+  this.router.navigate([rota])
+  console.log('cliquei');
+
+}
 }
